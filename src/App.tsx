65d5f361a33873c940095e697459d7abcb1d182d
Harsh Nilesh Toshniwal <htoshniw@usc.edu>
2025-11-15 17:15:55 -0800

    Changed the base router path and added deploment yaml file

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ const DashboardWrapper = styled.div`
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <AppContainer>
         <Header />
         <Routes>
